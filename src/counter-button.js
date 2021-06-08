7ca02a38ea9bdef712ece637fc52cd75e72a5a9e
Rajat <rajat.panwar@ril.com>
2021-06-08 23:00:30 +0530

    functional changes in input function

--- src/counter-button.js
+++ src/counter-button.js
@@ -3,7 +3,7 @@ import './counter-button.css'
 
 function CounterButton (props) {
     const { start=1, lowerLimit = 0, upperLimit = 1000 } = props
-    const [counter, setCounter] = useState(start)
+    const [counter, setCounter] = useState(Number(start))
     
     const decrement = () => {
         if(counter-1 >= Number(lowerLimit)) {
@@ -15,15 +15,20 @@ function CounterButton (props) {
             setCounter(counter+1)
         }
     }
-    const changeCounterValue = (value) => {
-        if(value <= upperLimit) {
-            setCounter(Number(value))
+    const changeCounterValue = (e) => {
+        const value = +Number(e.target.value)
+        if(e.target.value === '') {
+            setCounter(0)
+        }
+        if(isNaN(value)) {return}
+        if(value && Number(value) <= upperLimit) {
+            setCounter(value)
         }
     }
 return (
 <div className="counter-button-container">
     <button className="decrement-button" onClick={decrement}>-</button>
-    <input type="number" value={counter} max={upperLimit} min={lowerLimit} className="input-box" onChange={(e) => { changeCounterValue(e.target.value)}} onBlur={(e) => { if(!e.target.value) setCounter(start);}}/>
+    <input type="tel" value={counter} max={upperLimit} className="input-box" onChange={(e) => { changeCounterValue(e)}} />
     <button className="increment-button" onClick={increment}>+</button>
 </div>
 )
